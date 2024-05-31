15c404c8436782bd2a271e4e8c4d9cc0e7e0f7a0
Yjiie <andre.kiseleov@yandex.ru>
2024-05-31 18:02:34 +0400

    6 Laba.py

--- 6 Laba.py
+++ 6 Laba.py
@@ -1,71 +1,124 @@
-'''
-Задание состоит из двух частей. 1 часть – написать программу в соответствии со своим вариантом задания. Написать 2 варианта формирования (алгоритмический и с помощью функций Питона), сравнив по времени их выполнение. 2 часть – усложнить написанную программу, введя по своему усмотрению в условие минимум одно ограничение на характеристики объектов (которое будет сокращать количество переборов) и целевую функцию для нахождения оптимального решения.
+"""
+Есть требования:
+1 часть – написать программу в соответствии со своим вариантом задания.
+Написать 2 варианта формирования (алгоритмический и с помощью функций Питона), сравнив по времени их выполнение.
+2 часть – усложнить написанную программу введя по своему усмотрению в условие минимум одно ограничение на характеристики
+объектов (которое будет сокращать количество переборов) и целевую функцию для нахождения оптимального решения.
 
-Пароль состоит из 6 символов. Первые 2 – латинские буквы, остальные - латинские буквы или цифры. Составьте все возможные пароли.
-1й символ - латинская буква, 2ой латинская буква или цифра
-'''
-
-import string
+На плоскости задано К точек. Сформировать все возможные варианты выбора множества точек из них на проверку того, что они являются вершинами квадрата.
+"""
+from itertools import combinations
 import timeit
-import itertools
 
-# 1 часть
+nmax = 20  # макс. кол.точек
 
-# Алгоритмический метод формирования
-def generate_password_algorithm(num_passwords):
-    passwords = []
-    alphabet = string.ascii_letters + string.digits
-    for i in range(num_passwords):
-        first_char = string.ascii_letters[i % len(string.ascii_letters)]    # Первый символ латинская буква
-        second_char = alphabet[(i + 1) % len(alphabet)]                     # Второй символ либо цифра, либо буква
-        password = first_char + second_char
-        passwords.append(password)
-    return passwords
 
-# Метод формирования с помощью функций питона (с помощью itertools)
-def generate_passwords_pythonic():
-    passwords = []
-    alphabet = string.ascii_letters + string.digits
-    combinations = itertools.product(string.ascii_letters, alphabet)
-    for combination in combinations:
-        password = ''.join(combination)
-        passwords.append(password)
-    return passwords
+# является ли квадратом
+def square(a, b, c, d):
+    s1 = (a['x'] - b['x']) ** 2 + (a['y'] - b['y']) ** 2
+    s2 = (a['x'] - c['x']) ** 2 + (a['y'] - c['y']) ** 2
+    s3 = (a['x'] - d['x']) ** 2 + (a['y'] - d['y']) ** 2
+    s4 = (b['x'] - c['x']) ** 2 + (b['y'] - c['y']) ** 2
+    s5 = (b['x'] - d['x']) ** 2 + (b['y'] - d['y']) ** 2
+    s6 = (c['x'] - d['x']) ** 2 + (c['y'] - d['y']) ** 2
+    return ((s1 == s3) and (s1 == s4) and (s1 == s6) and (s2 == s5) and (s2 == 2 * s1)) \
+        or ((s1 == s2) and (s1 == s5) and (s1 == s6) and (s3 == s4) and (s3 == 2 * s1)) \
+        or ((s2 == s3) and (s2 == s4) and (s2 == s5) and (s1 == s6) and (s1 == 2 * s2))
 
-generated_passwords = generate_passwords_pythonic()
 
-print("1 часть: Генерация паролей до усложнения:","\nВсе возможные комбинации:",generated_passwords, "\nКоличество комбинаций до усложнения:",len(generated_passwords))  # - вывод всех комбинаций паролей (3224) до усложнения
+n = 0
+while n not in range(4, nmax + 1):
+     n = int(input(f"Количество точек от 4 до {nmax} n = "))
 
-time_taken_generate_password_algorithm = timeit.timeit(lambda: generate_password_algorithm(10000), number=1)
-print("Время выполнения алгоритмической функции:", time_taken_generate_password_algorithm)
+t = [{'x': 0, 'y': 0} for _ in range(n)]  # массив точек
 
-time_taken_generate_passwords_pythonic = timeit.timeit(generate_passwords_pythonic, number=1)
-print("Время выполнения с помощью функций Питона:", time_taken_generate_passwords_pythonic)
+print("Введите координаты", n, "точек, целые числа:")
+for i in range(n):
+    print("Точка", i + 1)
+    t[i]['x'] = int(input("x="))
+    t[i]['y'] = int(input("y="))
 
-def optimal_approach():
-    if time_taken_generate_password_algorithm < time_taken_generate_passwords_pythonic:
-        print ("Алгоритмический подход быстрее")
-    elif time_taken_generate_password_algorithm > time_taken_generate_passwords_pythonic:
-        print("Подход с использованием функций Питона быстрее")
-    else:
-        print("Оба подхода выполняются с одинаковой скоростью")
-optimal_approach()
 
-# 2 часть. Ограничение - первый символ латинская буква, а второй символ только четная цифра.
+def find_square_alg(t):
+    s_list = []
+    n_list = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                for l in range(k + 1, n):
+                    if square(t[i], t[j], t[k], t[l]):
+                        s_list.append([[t[i]['x'], t[i]['y']],
+                                       [t[j]['x'], t[j]['y']],
+                                       [t[k]['x'], t[k]['y']],
+                                       [t[l]['x'], t[l]['y']]])
+                    else:
+                        n_list.append([[t[i]['x'], t[i]['y']],
+                                       [t[j]['x'], t[j]['y']],
+                                       [t[k]['x'], t[k]['y']],
+                                       [t[l]['x'], t[l]['y']]])
+    return s_list, n_list
 
-def generate_password_with_constraint():
-    passwords = []
-    alphabet = string.ascii_letters
-    even_digits = '02468'
-    combinations = itertools.product(alphabet, even_digits)
-    for combo in combinations:
-        password = ''.join(combo)
-        passwords.append(password)
-    return passwords
 
-generated_passwords_2 = generate_password_with_constraint()
+def find_square_py(t):
+    s_list = []
+    n_list = []
+    for combination in list(combinations(t, 4)):
+        if square(combination[0], combination[1], combination[2], combination[3]):
+            s_list.append([[combination[0]['x'], combination[0]['y']],
+                           [combination[1]['x'], combination[1]['y']],
+                           [combination[2]['x'], combination[2]['y']],
+                           [combination[3]['x'], combination[3]['y']]])
+        else:
+            n_list.append([[combination[0]['x'], combination[0]['y']],
+                           [combination[1]['x'], combination[1]['y']],
+                           [combination[2]['x'], combination[2]['y']],
+                           [combination[3]['x'], combination[3]['y']]])
+    return s_list, n_list
 
-print("\n2 часть: Генерация паролей с учетом ограничения", "\nВсе возможные комбинации:",generated_passwords_2, "\nКоличество комбинаций после усложнения:",len(generated_passwords_2))
 
-time_taken_generate_password_with_constraint = timeit.timeit(lambda: generate_password_with_constraint, number=1)
-print("Время выполнения с учетом ограничения:", time_taken_generate_password_with_constraint)
+start_time = timeit.timeit()
+square_list, no_square_list = find_square_alg(t)
+algorithmic_time = timeit.timeit() - start_time
+print('Точки образующие квадрат(алгоритмический подход): ')
+print(*square_list, sep='\n')
+print('Точки не образующие квадрат(алгоритмический подход): ')
+print(*no_square_list, sep='\n')
+print('Время работы алгоритмического алгоритма: ', algorithmic_time)
+
+start_time = timeit.timeit()
+square_list, no_square_list = find_square_py(t)
+pythonic_time = timeit.timeit() - start_time
+print('Точки образующие квадрат(python функция): ')
+print(*square_list, sep='\n')
+print('Точки не образующие квадрат(python функция): ')
+print(*no_square_list, sep='\n')
+print('Время работа алгоритма с python функцией: ', pythonic_time)
+
+
+def find_square_opt(t):
+    print('lox')
+    s_list = []
+    n_list = []
+    filtered_t = list(filter(lambda p: p['x'] % 2 == 0 and p['y'] % 2 == 0, t))
+    for combination in list(combinations(filtered_t, 4)):
+        if square(combination[0], combination[1], combination[2], combination[3]):
+            s_list.append([[combination[0]['x'], combination[0]['y']],
+                           [combination[1]['x'], combination[1]['y']],
+                           [combination[2]['x'], combination[2]['y']],
+                           [combination[3]['x'], combination[3]['y']]])
+        else:
+            n_list.append([[combination[0]['x'], combination[0]['y']],
+                           [combination[1]['x'], combination[1]['y']],
+                           [combination[2]['x'], combination[2]['y']],
+                           [combination[3]['x'], combination[3]['y']]])
+    return s_list, n_list
+
+
+start_time = timeit.timeit()
+square_list, no_square_list = find_square_opt(t)
+pythonic_time_opt = timeit.timeit() - start_time
+print('Точки (с четными координатами) образующие квадрат: ')
+print(*square_list, sep='\n')
+print('Точки (с четными координатами) не образующие квадрат: ')
+print(*no_square_list, sep='\n')
+print('Время работы оптимального алгоритма: ', pythonic_time_opt)
